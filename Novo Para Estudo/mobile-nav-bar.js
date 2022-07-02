407621dd291040641416a3550501983ed58061a0
O Maldito Cobra <lollypoppy47@outlook.com>
2022-07-02 04:47:01 -0300

    Tudo Novamente

---
+++ Novo Para Estudo/mobile-nav-bar.js
@@ -0,0 +1,16 @@
+
+let borom = window.document.getElementById('btn-mobile')
+let LML = window.document.getElementById('navga')
+    function Clicar(){
+        if(LML.classList.contains("active")) {
+             LML.classList.remove('active')
+        }
+    }
+    function ALA() {
+    if(LML.classList.contains("active" )) {
+         LML.classList.remove('active' )
+    }
+    else {LML.classList.add('active' )}
+
+    }
+   
