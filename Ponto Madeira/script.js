c20fb950e6fc75c1d1b2a25bf58beea33822ac48
William Ramos <lollypoppy47@outlook.com>
2023-10-11 15:02:25 -0300

    JS DDs
    Adicionei alguns codigos de java script no Banner da Home do site, configurei validação no cadastro

--- Ponto Madeira/script.js
+++ Ponto Madeira/script.js
@@ -1,3 +1,13 @@
-function onclick(){
-    
+const iframes = document.querySelectorAll('iframe');
+const links = document.querySelectorAll('.btns a');
+let currentFrameIndex = 0;
+
+function changeIframe() {
+  iframes[currentFrameIndex].style.display = 'none';
+  currentFrameIndex = (currentFrameIndex + 1) % iframes.length;
+  iframes[currentFrameIndex].style.display = 'block';
+  links[currentFrameIndex].click();
 }
+
+setInterval(changeIframe, 2000);
+changeIframe();
